9cb8f44cdf897ba79b7126abc335fcaaee230e88
Henry Wu <h27wu@uwaterloo.ca>
2024-10-17 00:48:48 -0400

    create container for icons, update content, fixed arrow bug

--- index_files/animation.js
+++ index_files/animation.js
@@ -12,3 +12,22 @@ for (i = 0; i < cards.length; i++) {
     }
   });
 }
+
+const scrollArrow = document.getElementById('scroll-arrow');
+const nextSection = document.getElementById('about');
+
+function checkScroll() {
+    const sectionRect = nextSection.getBoundingClientRect();
+
+    if (sectionRect.top <= window.innerHeight && sectionRect.bottom >= 0) {
+      scrollArrow.classList.add('hidden');
+  } else {
+      if (window.scrollY === 0) {
+          scrollArrow.classList.remove('hidden');
+      }
+  }
+}
+
+window.addEventListener('scroll', checkScroll);
+
+checkScroll();
